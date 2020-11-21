4dfffe9fb1adc1ae321c6a4afe2e61d3cf4e6a05
QIU <2304735480@qq.com>
2020-11-21 16:03:08 +0800

    身份认证完成，数据转发部分完成，缺中心服务器

--- Edgeserver-springboot/springboot-api/src/main/java/com/cug/springapi/controller/DataTransmit.java
+++ Edgeserver-springboot/springboot-api/src/main/java/com/cug/springapi/controller/DataTransmit.java
@@ -24,7 +24,7 @@ import java.util.Map;
 
 public class DataTransmit {
 
-    static String adress = null;
+    static String videoPath = null;
     static String msg = null;
 
     @RequestMapping(value = "/datatransmit",method = RequestMethod.POST)
@@ -43,34 +43,43 @@ public class DataTransmit {
             //上传异常！
             msg = "上传失败";
         }
+        switch (msg){
+            case "上传成功":
+                //2.ImageDecrypt,解密视频,videoPath是解密后的视频地址
+                try {
+                    videoPath = ImageDecrypt.main();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-        //2.ImageDecrypt,解密视频,adress是解密后的视频地址
-        try {
-             adress = ImageDecrypt.main();
-        } catch (Exception e) {
-            e.printStackTrace();
+                //3.ImageTransmit,传入解密视频路径和中心服务器ip,发送至中心服务器
+
+                //4.TransimitResponse，接收中心服务器的响应
+                //无中心服务器，暂时跳过3、4步
+
+                //5.TokenCreate，生成Token
+                Object jsonToken = TokenCreate.main();
+
+                //6.object返回Token及消息
+                JSONObject object = new  JSONObject();
+                object.putAll((Map<? extends String, ? extends Object>) jsonToken);
+                object.put("code","200");
+                object.put("message",msg +"，解密成功！");
+                object.put("videoPath",videoPath);
+                return object;
+
+
+            case "上传失败":
+                JSONObject object1 = new  JSONObject();
+                object1.put("code","200");
+                object1.put("message",msg);
+                return object1;
+
+            default:
+                JSONObject object2 = new  JSONObject();
+                object2.put("code","200");
+                object2.put("message",msg);
+                return object2;
         }
-
-        //3.ImageTransmit,传入解密视频路径和中心服务器ip,发送至中心服务器
-/*        try {
-            Transmit.start(adress,"127.0.0.1");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        //4.TransimitResponse
-
-
-        //5.TokenCreate，生成Token
-        Object jsonToken = TokenCreate.main();
-
-        //6.object返回Token及消息
-        JSONObject object = new  JSONObject();
-        object.putAll((Map<? extends String, ? extends Object>) jsonToken);
-        object.put("code","200");
-        object.put("message",msg);
-        object.put("adress",adress);
-        return object;
     }
-
 }
